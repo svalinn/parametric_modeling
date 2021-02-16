dfdc85388ed2301393a27e1e2f1fdb8d0d12b941
Connor Moreno <camoreno@wisc.edu>
2021-02-15 21:39:20 -0800

    Make suggested changes to tokamak.py

--- tokamak.py
+++ tokamak.py
@@ -16,41 +16,37 @@ def parse_args():
 def read_radial_build(file):
 	
 	radial_build = yaml.load(file, Loader=yaml.FullLoader)
-	n = list(radial_build.keys())
-	t = list(radial_build.values())
-	return n, t
+	return radial_build
 
 
-def build_torus(R,r,n,t):
+def build_torus(major_radius,minor_radius,radial_build):
 
-	T = 0.0
-	for i in t:
-		T += i
-	if T+r > R:
-		print("Error: inboard thickness plus plasma minor radius is greater than plasma major radius.")
-		exit()
-	print(T)
+	layers = list(radial_build.keys())
+	thicknesses = list(radial_build.values())
+
+	if np.sum(thicknesses) > major_radius:
+		raise(ValueError, "material overlap at tokamak center.")
+	
 	cubit.init([''])
 
-	cubit.torus(R,r)
-	a = r
-	for j in t:
-		a += j
-		cubit.torus(R,a)
+	cubit.torus(major_radius,minor_radius)
+	
+	radii = np.cumsum(thicknesses) + minor_radius
+	for radius in radii:
+		cubit.torus(major_radius,radius)
 
-	k = len(t)+1
-	while k > 1:
-		cubit.cmd("subtract volume "+str(k-1)+" from volume "+str(k)+" keep_tool")
-		k -= 1
+	cubit.cmd("group 'mat:Vacuum' add vol 1")
+	vol_id = len(thicknesses) + 2
+	for id, layer_name in zip(range(vol_id-1,1,-1), reversed(layers)):
+		print(id)
+		print(layer_name)
+		print(vol_id)
+		cubit.cmd("subtract volume "+str(id - 1)+" from volume "+str(id)+" keep_tool")
+		cubit.cmd("group 'mat:"+ layer_name +"' add vol " + str(vol_id))
+		vol_id += 1
 
 	cubit.cmd("imprint volume all")
 	cubit.cmd("merge volume all")
-
-	cubit.cmd("group 'mat:Vaccum' add vol 1")
-	m = 2*len(n)+1
-	for l in n:
-		cubit.cmd("group 'mat:"+str(l)+"' add vol "+str(m))
-		m -= 1
 
 
 def save_geometry(export_path):
@@ -63,7 +59,7 @@ def main():
 
 	parameters = parse_args()
 	radial_build = read_radial_build(parameters.radial_build)
-	build_torus(parameters.major_radius, parameters.minor_radius, radial_build[0], radial_build[1])
+	build_torus(parameters.major_radius, parameters.minor_radius, radial_build)
 	save_geometry(parameters.export_path)
 	print("Successfully created tokamak geometry.")
 
